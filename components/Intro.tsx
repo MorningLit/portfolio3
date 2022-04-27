bcfde0ad2823fc20d801a20f3eec344f2f5c456f
MorningLit <ambroseliew1998@gmail.com>
2022-04-27 15:47:42 +0800

    Set up first component

---
+++ components/Intro.tsx
@@ -0,0 +1,111 @@
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  Grid,
+  GridItem,
+  StackDivider,
+  useColorModeValue,
+  VisuallyHidden,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+
+export default function Intro() {
+  return (
+    <Container maxW={"7xl"}>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6} py={{ base: 18, md: 24 }}>
+        <GridItem colSpan={{ base: 3, lg: 1 }}>
+          <Image
+            rounded={"md"}
+            alt={"Ambrose's Photo"}
+            src={"/Ambrose.jpg"}
+            fit={"cover"}
+            align={"center"}
+            w={"100%"}
+            h={{ base: "100%", lg: "500px" }}
+          />
+        </GridItem>
+        <GridItem colSpan={{ base: 3, lg: 2 }}>
+          <Stack spacing={{ base: 6, md: 10 }}>
+            <Box as={"header"}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
+              >
+                Ambrose Liew
+              </Heading>
+              <Text
+                color={useColorModeValue("gray.900", "gray.400")}
+                fontWeight={300}
+                fontSize={"2xl"}
+              >
+                Frontend Engineer
+              </Text>
+            </Box>
+
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={"column"}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue("gray.200", "gray.600")}
+                />
+              }
+            >
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                <Text
+                  color={useColorModeValue("gray.500", "gray.400")}
+                  fontSize={"2xl"}
+                  fontWeight={"300"}
+                >
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore
+                </Text>
+                <Text fontSize={"lg"}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                  aliquid amet at delectus doloribus dolorum expedita hic, ipsum
+                  maxime modi nam officiis porro, quae, quisquam quos
+                  reprehenderit velit? Natus, totam.
+                </Text>
+              </VStack>
+              <Box>
+                <Text
+                  fontSize={{ base: "16px", lg: "18px" }}
+                  color={useColorModeValue("yellow.500", "yellow.300")}
+                  fontWeight={"500"}
+                  textTransform={"uppercase"}
+                  mb={"4"}
+                >
+                  Skills
+                </Text>
+
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                  <List spacing={2}>
+                    <ListItem>Chronograph</ListItem>
+                    <ListItem>Master Chronometer Certified</ListItem>{" "}
+                    <ListItem>Tachymeter</ListItem>
+                  </List>
+                  <List spacing={2}>
+                    <ListItem>Anti‑magnetic</ListItem>
+                    <ListItem>Chronometer</ListItem>
+                    <ListItem>Small seconds</ListItem>
+                  </List>
+                </SimpleGrid>
+              </Box>
+            </Stack>
+          </Stack>
+        </GridItem>
+      </Grid>
+    </Container>
+  );
+}
