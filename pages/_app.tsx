7676f2340e66ac674389d8e513fb5d3185987736
MorningLit <ambroseliew1998@gmail.com>
2022-04-30 15:51:47 +0800

    Add dark theme by default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../utils/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Head>
         <title>Ambrose Liew</title>
         <link
