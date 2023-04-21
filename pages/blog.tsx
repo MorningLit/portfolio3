a8281f202c03f205094d3650c371247a5ab6be7f
Ambrose <ambroseliew1998@gmail.com>
2023-04-21 17:58:29 +0800

    Add temp pages for blog and others

---
+++ pages/blog.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Blog: NextPage = () => {
+  return <>Work in progress! Stay tuned!</>;
+};
+
+export default Blog;
---
+++ pages/others.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Others: NextPage = () => {
+  return <>Work in progress! Stay tuned!</>;
+};
+
+export default Others;
