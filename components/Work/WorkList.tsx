c9b504b9c78699c2a346428de3f34f1d95331493
Ambrose <ambroseliew1998@gmail.com>
2023-04-28 20:55:51 +0800

    Update work and resume

--- components/Work/WorkList.tsx
+++ components/Work/WorkList.tsx
@@ -12,13 +12,13 @@ const WorkList = () => {
         companyName="ByteDance"
         description={[
           "Utilised TypeScript, React, CSS and coordinated with various teams to fulfil product requirements.",
-          "Designed an interactive graph application using React-flow-renderer.",
-          "Contributed to and resolved issues in internal custom React Hooks library.",
+          "Contributed 7 commits, and resolved 23 issues in internal custom React Hooks library, which is used in majority of company's projects.",
+          "Designed an interactive graph node application using React-flow-renderer.",
+          "Designed a graph chart using Apache ECharts in React.",
         ]}
         tags={["Frontend"]}
         startDate={new Date("2022-05")}
-        endDate={new Date("2022-10")}
-        current
+        endDate={new Date("2022-11")}
         heights={["80px", "150px", "90px", "60px"]}
       />
       <Work
